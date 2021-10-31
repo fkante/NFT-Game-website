b24b0fd53ccdacef115b462303689e8ee5f9bfa8
Francis KANTE <42733388+fkante@users.noreply.github.com>
2021-10-31 09:03:19 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x5ce8e2B3695665FD91dF29a6D84618deb0B87504';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
